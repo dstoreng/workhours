4957d0bde3cb3f5858dcb66c0a95f4d59309d265
dstoreng <danielstoreng@gmail.com>
2014-05-29 16:02:42 +1200

    Fjerning av unødvendige ting

--- WorkHours/src/com/example/workhours/util/EventAdapter.java
+++ WorkHours/src/com/example/workhours/util/EventAdapter.java
@@ -48,7 +48,7 @@ public class EventAdapter extends ArrayAdapter<Shift> {
 	        to.setText(s.getToFormatted());
 	        
 	        TextView hours = (TextView) convertView.findViewById(R.id.tvHours);
-	        hours.setText(Integer.toString(s.getHours()) + " ID: "  +Integer.toString(s.getId()));
+	        hours.setText(Integer.toString(s.getHours()));
 	        
 	        if(showImage){
 		        ImageView conf = (ImageView) convertView.findViewById(R.id.tvConfirmed);
