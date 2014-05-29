988cba933d4d72b799be1eedb2d6f86e3de72db2
dstoreng <danielstoreng@gmail.com>
2014-05-29 15:48:55 +1200

    smårips

--- WorkHours/src/com/example/workhours/fragments/ProfileFragmentDetails.java
+++ WorkHours/src/com/example/workhours/fragments/ProfileFragmentDetails.java
@@ -14,8 +14,8 @@ public class ProfileFragmentDetails extends Fragment {
 	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                             Bundle savedInstanceState) {
 	        // Inflate the layout for this fragment
-	      //  return inflater.inflate(R.layout.fragment_profile_details, container, false);
-		 return inflater.inflate(R.layout.test, container, false);
+	      	return inflater.inflate(R.layout.fragment_profile_details, container, false);
+		 //return inflater.inflate(R.layout.test, container, false);
 	    }
 
 }
